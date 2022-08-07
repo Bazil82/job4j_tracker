1c28c0a4d144adabcb39b5c1f47ce02ac21c6a05
Bazil82 <program1408@gmail.com>
2022-08-07 12:40:35 +0300

    Исправление CI Bot

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -1,10 +1,10 @@
 package ru.job4j.pojo;
 
 public class Shop {
-    public static int indexOfNull(Product[] products) {
+    public static int indexOfNull(Product[] foods) {
         int res = -1;
-        for (int i = 0; i < products.length; i++) {
-            if (products[i] == null) {
+        for (int i = 0; i < foods.length; i++) {
+            if (foods[i] == null) {
                 res = i;
                 break;
             }
